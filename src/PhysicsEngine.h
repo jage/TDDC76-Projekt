6d45cf626486085c97e4649f5e502ed3500e3f1d
jonas <jonas@laptop.(none)>
2009-11-24 15:12:32 +0100

    PhysicsEngine

---
+++ src/PhysicsEngine.h
@@ -0,0 +1,13 @@
+#ifndef PHYSICSENGINE_H_
+#define PHYSICSENGINE_H_
+
+class PhysicsEngine {
+	
+	public:
+	void calc_new_pos(vector<element> e_vec)
+	void calc_new_pos(element e)
+	
+	private:
+	
+}
+#endif /*PHYSICSENGINE_H_*/
