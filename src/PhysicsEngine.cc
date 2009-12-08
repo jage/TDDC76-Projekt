c1161330315a058248e757a5a2012e720ed469f1
jonas <jonas@laptop.(none)>
2009-12-08 20:53:49 +0100

    PhysicsEngine

--- src/PhysicsEngine.cc
+++ src/PhysicsEngine.cc
@@ -1,29 +1,30 @@
 #include "PhysicsEngine.h"
 #include <math.h>
+#include <stdlib.h>
 
+
+double dt_ = 0.0125;
 /*
  * update_pos beräknar den nya positionen för ett givet MovableElement-objekt samt en given gravitation.
  */
-void PhysicsEngine::update_pos(MovableElement* element, const double gravity, const double wind) {
+void PhysicsEngine::update_pos(MovableElement* element, const double& gravity, const double& wind) {
 	update_dx(element, wind);
 	update_dy(element, gravity);
-	update_x(element, gravity);
-	update_y(element,gravity);
+	update_x(element);
+	update_y(element);
+}
+
+void PhysicsEngine::update_x(MovableElement* element){
+	element->set_x(element->get_x() + element->get_dx() * dt_);
+}
+void PhysicsEngine::update_y(MovableElement* element){
+	element->set_y(element->get_y() + element->get_dy() * dt_);
 }
 
 void PhysicsEngine::update_dx(MovableElement* element, const double wind){
-	//wind_effect(element, gravity);
-}
-void PhysicsEngine::update_dy(MovableElement* element, const double gravity){
-	element->set_dy( sqrt( pow(element->get_dy(),2) - 2 * gravity * element->get_dy() ));
-}
-void PhysicsEngine::update_x(MovableElement* element, const double gravity){
-	element->set_x(element->get_x() + element->get_dx());
-}
-void PhysicsEngine::update_y(MovableElement* element, const double gravity){
-	element->set_y(element->get_y() + element->get_dy());
+	element->set_dx(element->get_dx() - wind * abs(element->get_dx()) * element->get_windFactor() * dt_);
 }
 
-void PhysicsEngine::wind_effect(MovableElement* element, const double wind){
-	element->set_dx(element->get_dx() *(1 - wind) * (1 - (element->get_y()/ 10000)));
+void PhysicsEngine::update_dy(MovableElement* element, const double gravity){
+	element->set_dy(element->get_dy() - gravity * dt_ * 100);
 }
