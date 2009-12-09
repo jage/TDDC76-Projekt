a8c67b784b29c7d2e27b66d86c9d0dce97ab90f6
Viktor Deleskog <v.deleskog@gmail.com>
2009-12-09 16:27:38 +0100

    Interval intersect()

--- src/Interval.cc
+++ src/Interval.cc
@@ -8,7 +8,21 @@ const bool Interval::belongs(const int& value) const
 	return (value>=lower_ && value<=upper_);
 }
 
+const bool Interval::intersect(const Interval& value) const
+{
+	return belongs(value.lower_) || belongs(value.upper_);	
+}
+
 const int Interval::get_middle() const
 {
 	return (int)((upper_+lower_)*0.5);	
+}
+
+const std::string Interval::str() const
+{
+	std::ostringstream os;
+
+	os << "lower: " << lower_ << " upper: " << upper_;
+
+	return os.str();
 }
--- src/Interval.h
+++ src/Interval.h
@@ -1,12 +1,17 @@
 #ifndef INTERVAL_H_
 #define INTERVAL_H_
 
+#include <string>
+#include <sstream>
+
 class Interval
 {
 public:
 	Interval(const int&,const int&);
 	const bool belongs(const int&) const;
+	const bool intersect(const Interval&) const;
 	const int get_middle() const;
+	const std::string str() const;
 	virtual ~Interval() {};
 private:
 	int lower_;
