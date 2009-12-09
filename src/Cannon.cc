b13cbd67fd9324298b0719e06b93c18a05ee15cc
rosjo <rosjo@unicorn.(none)>
2009-12-09 15:45:49 +0100

    Ingen fireangle i cannon

--- src/Cannon.cc
+++ src/Cannon.cc
@@ -2,22 +2,14 @@
 #include "Enums.h"
 
 Cannon::Cannon(Ammunition* ptrAmmo)
-	:Element(50,58, CANNON),fireAngle_(0),power_(0),ptr_ammunition_(ptrAmmo)	{}
+	:Element(50,58, CANNON),power_(0),ptr_ammunition_(ptrAmmo)	{}
 	
-void Cannon::set_angle(const double& value)
-{
-	fireAngle_=value;
-}
 
 void Cannon::adjust_angle(const double& delta)
 {
-	fireAngle_+=delta;
+	angle_+=delta;
 }
 
-const double Cannon::get_angle() const
-{
-	return fireAngle_;	
-}
 
 void Cannon::set_power(const int& value)
 {
--- src/Cannon.h
+++ src/Cannon.h
@@ -8,9 +8,7 @@ class Cannon : public Element
 {
 public:
 	Cannon(Ammunition* ammo=0);
-	void set_angle(const double&);	// sets fire angle
 	void adjust_angle(const double&); // increase/decrease fire angle
-	const double get_angle() const;
 	void set_power(const int&);
 	const double get_power() const;
 	void change_ammunition(Ammunition*); // change ammunition
@@ -19,7 +17,6 @@ public:
 	const bool fired() const; // is the cannon fired?
 	virtual ~Cannon();
 private:
-	double fireAngle_;
 	int power_;
 	bool fired_;
 	Ammunition* ptr_ammunition_; // pointer to current ammunition object 
