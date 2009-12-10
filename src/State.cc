3c07cc4e55aaf70c3acdb096d085830f19b267a2
rosjo <rosjo@unicorn.(none)>
2009-12-10 23:10:06 +0100

    Inga egna grafik funktioner i State

--- src/State.cc
+++ src/State.cc
@@ -20,38 +20,6 @@ State::State(GraphicsEngine* graphicengine, GameWorld* gameworld, Audio* audio)
 
 State::~State() {}
 //--------------------------------------------------------------//
-
-
-
-//Tempor�r funktion Graphics engine b�r ta hand om allt
-SDL_Surface* load_image( std::string filename )
-	  {
-	      //Temporary storage for the image that's loaded
-	      SDL_Surface* loadedImage = NULL;
-
-	      //The optimized image that will be used
-	      SDL_Surface* optimizedImage = NULL;
-
-	      //Load the image
-	      loadedImage = IMG_Load( filename.c_str() );
-
-	      //If nothing went wrong in loading the image
-	      if( loadedImage != NULL )
-	      {
-	          //Create an optimized image
-	          optimizedImage = SDL_DisplayFormat( loadedImage );
-
-	          //Free the old image
-	          SDL_FreeSurface( loadedImage );
-	      }
-	      else
-	    	  cerr << "Image not found!" << endl;
-
-	      //Return the optimized image
-	      return optimizedImage;
-	  }
-
-
 //Meny-----------------------------------------------------------//
 
 //Meny::Meny(){}
