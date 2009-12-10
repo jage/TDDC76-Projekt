f0e8438cae0eaad883667e1c23646e9e34219a5c
jonas <jonas@laptop.(none)>
2009-12-10 11:54:05 +0100

    Kommentarer i PhysicsEngine

--- src/PhysicsEngine.h
+++ src/PhysicsEngine.h
@@ -11,10 +11,30 @@ class PhysicsEngine {
 		void update_pos(MovableElement*, const double&, const double&); 		//indata: (element, gravitation, wind)
 
 	private:
-		void update_x(MovableElement*);						//indata: (element, gravitation)
-		void update_y(MovableElement*);						//indata: (element, gravitation)
+		/*
+		* void update_x uppdaterar x-positionen hos ett Movable-element
+		*/
+		void update_x(MovableElement*);
+
+
+		/*
+		* void update_y uppdaterar y-positionen hos ett Movable-element
+		*/
+		void update_y(MovableElement*);
+
+		/*
+		* void update_dx uppdaterar dx-variabeln hos ett Movable-element
+		*/
 		void update_dx(MovableElement*, const double);
+
+		/*
+		* void update_dy uppdaterar dy-variabeln hos ett Movable-element
+		*/
 		void update_dy(MovableElement*, const double);
+
+		/*
+		* int return_sign returnerar tecknet på en int-variabel
+		*/
 		int return_sign(int);
 };
 #endif /*PHYSICSENGINE_H_*/
