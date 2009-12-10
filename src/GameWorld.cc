cbd729d189204e8930990a580ff94ed151202b23
Viktor Deleskog <v.deleskog@gmail.com>
2009-12-10 22:42:46 +0100

    Some nice de-fucking-forming!

--- src/GameWorld.cc
+++ src/GameWorld.cc
@@ -190,13 +190,8 @@ bool GameWorld::generate_world(const int& seed)
 	ptr_cannonL_->set_y(height_-startHeight-15);
 	ptr_cannonR_->set_y(height_-endHeight-15);
 	
-/*	 // test deform
-	 Collision testColl;
-	 
-	 srand(time(NULL));
-	 
-	 testColl.x= rand() % width_;
-	 testColl.blastRadius =70;
+	 // test deform
+	 Explosion testExpl(315,350,48);
 	 
 	Destructable* dp;
 	
@@ -206,10 +201,10 @@ bool GameWorld::generate_world(const int& seed)
 		Element* elem = *it;
 		dp = dynamic_cast<Destructable*>(elem);
 		
-		if (dp!=0) dp->deform(testColl);
+		if (dp!=0) dp->deform(testExpl);
 		
 		++it;
-	}*/
+	}
 	 		
 	delete []calculatedHeights;
 	return true;
